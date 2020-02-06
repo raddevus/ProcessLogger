831e62b598da9df07e5d0746a55f11529d41803c
raddevus <1radeutsch1@gmail.com>
2020-02-06 10:47:10 -0500

    Adding feature so user can provide 1 arg to process and get a matching record back.

--- ProcessLogger/ProcessLogger.cpp
+++ ProcessLogger/ProcessLogger.cpp
@@ -11,64 +11,37 @@
 #include "include\btrieveCpp.h"
 using namespace std;
 
-static char* btrieveFileName = (char*)"squaresAndSquareRoots.btr";
+// exe.info is the name of the file that will hold our data
 string exeInfoFileName = "exe.info";
 
-#define MIN_X 0
-#define MAX_X 255
 #pragma pack(1)
 
 int recordSize = 0;
 
 typedef struct {
-    uint8_t x;
-    uint16_t xSquared;
-    double xSquareRoot;
-} record_t;
-
-typedef struct {
-    char exeName[51];
-    char dateTime[20];
-    char userName[51];
+    char exeName[51]; // data + 1 for '\0'
+    char dateTime[20]; // data + 1 for '\0'
+    char userName[51]; // data + 1 for '\0'
 } executableInfo;
 
-const int TOTAL_EXEINFO_LENGTH = 122;
+// This is the total executableInfo Record size in bytes.
+const int TOTAL_EXEINFO_RECORD_LENGTH = 122;
 
 #pragma pack()
 // Disables a warning about using 
 // localtime, for our sample code this is fine
 #pragma warning(disable : 4996) 
 
-typedef uint8_t _key_t;
-
 string getDateTime() {
+    // ##### Provides a simple function which returns a 19-byte
+    // date-time formatted string for the current date-time
+    // Format looks like : MM-DD-YYYY HH:MM:SS
+    // Example :           02-06-2020 10:29:47
     auto t = std::time(nullptr);
     auto tm = *localtime(&t);
     std::ostringstream oss;
     oss << put_time(&tm, "%m-%d-%Y %H:%M:%S");
     return oss.str();
-}
-
-static Btrieve::StatusCode
-createFile(BtrieveClient* btrieveClient, const char* fileName)
-{
-    Btrieve::StatusCode status;
-    BtrieveFileAttributes btrieveFileAttributes;
-    // If SetFixedRecordLength() fails.
-    cout << getDateTime() << endl;
-    if ((status = btrieveFileAttributes.SetFixedRecordLength(sizeof(record_t))) != Btrieve::STATUS_CODE_NO_ERROR)
-    {
-        printf("Error: BtrieveFileAttributes::SetFixedRecordLength():%d:%s.\n", status, Btrieve::StatusCodeToString(status));
-        goto leave;
-    }
-    // If FileCreate() fails.
-    if ((status = btrieveClient->FileCreate(&btrieveFileAttributes, fileName, Btrieve::CREATE_MODE_OVERWRITE)) != Btrieve::STATUS_CODE_NO_ERROR)
-    {
-        printf("Error: BtrieveClient::FileCreate():%d:%s.\n", status, Btrieve::StatusCodeToString(status));
-        goto leave;
-    }
-leave:
-    return status;
 }
 
 static Btrieve::StatusCode
@@ -110,35 +83,12 @@ addRecordToInfoFile(BtrieveFile* btrieveFile, string exeName, string dateTime, s
     sprintf(record.dateTime, "%s",dateTime.c_str());
     sprintf(record.userName,"%s",userName.c_str());
     // If RecordCreate() fails.
-    if ((status = btrieveFile->RecordCreate((char*)&record, TOTAL_EXEINFO_LENGTH)) != Btrieve::STATUS_CODE_NO_ERROR)
+    if ((status = btrieveFile->RecordCreate((char*)&record, TOTAL_EXEINFO_RECORD_LENGTH)) != Btrieve::STATUS_CODE_NO_ERROR)
     {
         printf("Error: BtrieveFile::RecordCreate():%d:%s.\n", status, Btrieve::StatusCodeToString(status));
         goto leave;
     }
     
-leave:
-    return status;
-}
-
-
-static Btrieve::StatusCode
-loadFile(BtrieveFile* btrieveFile)
-{
-    Btrieve::StatusCode status = Btrieve::STATUS_CODE_NO_ERROR;
-    int i;
-    record_t record;
-    for (i = MIN_X; i <= MAX_X; i++)
-    {
-        record.x = (uint8_t)i;
-        record.xSquared = (uint16_t)(i * i);
-        record.xSquareRoot = sqrt((double)i);
-        // If RecordCreate() fails.
-        if ((status = btrieveFile->RecordCreate((char*)&record, sizeof(record))) != Btrieve::STATUS_CODE_NO_ERROR)
-        {
-            printf("Error: BtrieveFile::RecordCreate():%d:%s.\n", status, Btrieve::StatusCodeToString(status));
-            goto leave;
-        }
-    }
 leave:
     return status;
 }
@@ -158,70 +108,13 @@ leave:
 }
 
 static Btrieve::StatusCode
-deleteFile(BtrieveClient* btrieveClient)
-{
-    Btrieve::StatusCode status;
-    // If FileDelete() fails.
-    if ((status = btrieveClient->FileDelete(btrieveFileName)) != Btrieve::STATUS_CODE_NO_ERROR)
-    {
-        printf("Error: BtrieveClient::FileDelete():%d:%s.\n", status, Btrieve::StatusCodeToString(status));
-        goto leave;
-    }
-leave:
-    return status;
-}
-static Btrieve::StatusCode
-createIndex(BtrieveFile* btrieveFile)
-{
-    Btrieve::StatusCode status;
-    BtrieveIndexAttributes btrieveIndexAttributes;
-    BtrieveKeySegment btrieveKeySegment;
-    // If SetField() fails.
-    if ((status = btrieveKeySegment.SetField(0, 1, Btrieve::DATA_TYPE_UNSIGNED_BINARY)) != Btrieve::STATUS_CODE_NO_ERROR)
-    {
-        printf("Error: BtrieveKeySegment::SetField():%d:%s.\n", status, Btrieve::StatusCodeToString(status));
-        goto leave;
-    }
-    // If AddKeySegment() fails.
-    if ((status = btrieveIndexAttributes.AddKeySegment(&btrieveKeySegment)) != Btrieve::STATUS_CODE_NO_ERROR)
-    {
-        printf("Error: BtrieveIndexAttributes::AddKeySegment():%d:%s.\n", status, Btrieve::StatusCodeToString(status));
-        goto leave;
-    }
-    // If IndexCreate() fails.
-    if ((status = btrieveFile->IndexCreate(&btrieveIndexAttributes)) != Btrieve::STATUS_CODE_NO_ERROR)
-    {
-        printf("Error: BtrieveFile::IndexCreate():%d:%s.\n", status, Btrieve::StatusCodeToString(status));
-        goto leave;
-    }
-leave:
-    return status;
-}
-static Btrieve::StatusCode
-retrieveRecord(BtrieveFile* btrieveFile, _key_t* key)
-{
-    Btrieve::StatusCode status = Btrieve::STATUS_CODE_NO_ERROR;
-    record_t record;
-    // If RecordRetrieve() fails.
-    if (btrieveFile->RecordRetrieve(Btrieve::COMPARISON_EQUAL, Btrieve::INDEX_1, (char*)key, sizeof(*key), (char*)&record, sizeof(record)) != sizeof(record))
-    {
-        status = btrieveFile->GetLastStatusCode();
-        printf("Error: BtrieveFile::RecordRetrieve():%d:%s.\n", status, Btrieve::StatusCodeToString(status));
-        goto leave;
-    }
-    printf("record: (%u, %u, %f)\n", record.x, record.xSquared, record.xSquareRoot);
-leave:
-    return status;
-}
-
-static Btrieve::StatusCode
 createInfoFile(BtrieveClient* btrieveClient, const char* fileName)
 {
     Btrieve::StatusCode status = Btrieve::STATUS_CODE_NO_ERROR;;
     BtrieveFileAttributes btrieveFileAttributes;
     // If SetFixedRecordLength() fails.
     cout << getDateTime() << endl;
-    if ((status = btrieveFileAttributes.SetFixedRecordLength(TOTAL_EXEINFO_LENGTH)) != Btrieve::STATUS_CODE_NO_ERROR)
+    if ((status = btrieveFileAttributes.SetFixedRecordLength(TOTAL_EXEINFO_RECORD_LENGTH)) != Btrieve::STATUS_CODE_NO_ERROR)
     {
         printf("Error: BtrieveFileAttributes::SetFixedRecordLength():%d:%s.\n", status, Btrieve::StatusCodeToString(status));
         goto leave;
@@ -276,7 +169,7 @@ retrieveInfoRecord(BtrieveFile* btrieveFile, string key)
     if (btrieveFile->RecordRetrieve(Btrieve::COMPARISON_EQUAL,
         Btrieve::INDEX_1, keyCopy, 
         key.length() *sizeof(char), 
-        (char*)&record, TOTAL_EXEINFO_LENGTH ) != TOTAL_EXEINFO_LENGTH)//sizeof(record))
+        (char*)&record, TOTAL_EXEINFO_RECORD_LENGTH ) != TOTAL_EXEINFO_RECORD_LENGTH)//sizeof(record))
     {
         status = btrieveFile->GetLastStatusCode();
         printf("Error: BtrieveFile::RecordRetrieve():%d:%s.\n", status, Btrieve::StatusCodeToString(status));
@@ -289,8 +182,40 @@ leave:
     return status;
 }
 
+Btrieve::StatusCode getRecord(char * processName) {
+    BtrieveClient btrieveClient(0x4232, 0);
+    BtrieveFile btrieveFile;
+
+    Btrieve::StatusCode status = Btrieve::STATUS_CODE_NO_ERROR;
+    //If openFile() fails.
+    if ((status = openFile(&btrieveClient, &btrieveFile, exeInfoFileName.c_str())) != Btrieve::STATUS_CODE_NO_ERROR)
+    {
+        return status;
+    }
+    if ((status = retrieveInfoRecord(&btrieveFile, processName)) != Btrieve::STATUS_CODE_NO_ERROR)
+    {
+        return status;
+    }
+
+    if ((status = closeFile(&btrieveClient, &btrieveFile)) != Btrieve::STATUS_CODE_NO_ERROR)
+    {
+        return status;
+    }
+    return status;
+
+}
+
 int main(int argc, char* argv[])
 {
+
+    Btrieve::StatusCode status = Btrieve::STATUS_CODE_NO_ERROR;
+
+    
+    if (argc == 2) {
+        argv[1];
+        status = getRecord(argv[1]);
+        return 0;
+    }
 
     if (argc < 3 || argc > 3) {
         cout << "### Must provide two arguments.  ####" << endl;
@@ -298,6 +223,7 @@ int main(int argc, char* argv[])
         return -1;
     }
 
+    
     char* processName = argv[1];
     printf("%s\n", processName);
 
@@ -307,8 +233,7 @@ int main(int argc, char* argv[])
     BtrieveClient btrieveClient(0x4232, 0);
     BtrieveFile btrieveFile;
     //BtrieveFile bFile;
-
-    Btrieve::StatusCode status = Btrieve::STATUS_CODE_NO_ERROR;
+    
     if ((status = createInfoFile(&btrieveClient, exeInfoFileName.c_str())) != Btrieve::STATUS_CODE_NO_ERROR)
     {
         goto leave;
